9a887e73313694d9b71ec7a85aeab235eb2ae0af
Demetria Mack <Demetria.mack@bison.howard.edu>
2024-05-05 16:17:28 -0400

    Initial commit

---
+++ Assets/Navigation.cs
@@ -0,0 +1,36 @@
+// using System.Collections;
+// using System.Collections.Generic;
+// using UnityEngine;
+
+// public class Navigation : MonoBehaviour
+// {
+//     [SerializeField]
+//     private camera topDownCamera; 
+//     [SerializeField]
+//     private GameObject navTargetObject; 
+//     private UnityEngine.AI.NavMeshPath path; 
+//     private LineRender line; 
+//     private bool lineToggle = false; 
+
+//     // Start is called before the first frame update
+//     void Start()
+//     {
+//         path = new UnityEngine.AI.NavMeshPath(); 
+//         line = transform.GetComponent<LineRenderer>();
+//     }
+
+//     // Update is called once per frame
+//     void Update()
+//     {
+//         if((Input.touchCount > 0) && (Input.GetTouch(0).phase == TouchPhase.Began)){
+//             lineToggle =!lineToggle; 
+//         }
+
+//         if(lineToggle){
+//             NaMesh.CalculatePath(transform.position, navTargetObject.transform.position, UnityEngine.AI.NavMesh.AllAreas, path);
+//             line.positionCount = path.corners.Length; 
+//             line.SetPositions (path.corners);
+//             line.enabled = true; 
+//         }
+//     }
+// }
